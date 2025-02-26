af002442d639c81fdb8b8c4ae94da868ba934f30
yurihuku <mochizuki115sannko.gmail.com>
2025-02-26 07:56:11 +0000

    rough styling including header

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,66 +10,107 @@
 </head>
 
 <body>
-  <h1 class="title">{{ $post->title }}</h1>
-  <div class="content">
-    <div class="content__post">
-      <h3>質問内容</h3>
-      <p>{{ $post->body }}</p>
-    </div>
-    <div class="edit"><a href="/posts/{{ $post->id }}/edit">編集</a></div>
-  </div>
+  <x-app-layout>
+    <x-slot name="header">
+    </x-slot>
 
-  <!-- 共感ボタン -->
-  <div class="reactions">
-    <div class="like_reaction">
-      @auth
-      @if (Auth::user()->likes()->where('post_id', $post->id)->exists())
-      <ion-icon name="heart" class="like-btn cursor-pointer text-pink-500" id="{{$post->id}}_like"></ion-icon>
-      @else
-      <ion-icon name="heart-outline" class="like-btn cursor-pointer" id="{{$post->id}}_like"></ion-icon>
-      @endif
-      <p class="count-num">{{ $post->likes()->sum('count') }}</p>
-      @endauth
-    </div>
-    <div class="support_reaction">
-      @auth
-      @if (Auth::user()->supports()->where('post_id', $post->id)->exists())
-      <ion-icon name="flame" class="support-btn cursor-pointer text-pink-500" id="{{$post->id}}_support"></ion-icon>
-      @else
-      <ion-icon name="flame-outline" class="support-btn cursor-pointer" id="{{$post->id}}_support"></ion-icon>
-      @endif
-      <p class="count-num">{{ $post->supports()->sum('count') }}</p>
-      @endauth
-    </div>
-  </div>
+    <main class="mx-auto flex w-3/5 flex-wrap pt-10">
+      <!-- <h1 class="title">{{ $post->title }}</h1> -->
 
-  <form action="/posts/reply" method="POST">
-    @csrf
-    <div class="comment">
-      <input type="text" name="post_reply[comment]" placeholder="コメント入力" value="{{ old('post_reply.comment') }}">
-      <input type="hidden" name="post_reply[post_id]" value="{{ $post->id }}">
-      <p class="comment_error" style="color:red">{{ $errors->first('post_reply.comment') }}</p>
-    </div>
-    <input class="cursor-pointer" type="submit" value="返信">
-  </form>
+      <div class="flex-1/2">
+        <div class="content border border-gray-300 rounded-xl p-5">
+          <div class="post__user flex gap-2.5 text-xs">
+            <p>profile-pic</p>
+            <p class="name">{{$post->user->name}}</p>
+            <p class="country">{{$post->user->country->name}}</p>
+            <p class="start__date">{{$post->user->study_abroad_start_date}} ~</p>
+          </div>
+          <p class="mt-4">{{ $post->body }}</p>
+          <p class="mt-4 text-right text-xs">{{$post->created_at}}</p>
+          <div class="flex justify-end gap-2">
+            <div class="edit"><a href="/posts/{{ $post->id }}/edit"><ion-icon name="pencil-outline"></ion-icon></a></div>
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+              @csrf
+              @method('DELETE')
+              <button type="button" onclick="deleteQuestion({{ $post->id }})"><ion-icon name="trash-outline"></ion-icon></button>
+            </form>
+          </div>
+          <!-- <div class="edit"><a href="/posts/{{ $post->id }}/edit">編集</a></div> -->
+        </div>
 
-  <div class="post_reply">
-    @foreach ($post->post_replies as $post_reply)
-    <div>
-      <p>{{ $post_reply->user->name }}</p>
-      <p>{{ $post_reply->comment }}</p>
-    </div>
-    @endforeach
-  </div>
+        <!-- 共感ボタン -->
+        <div class="reactions flex justify-center gap-2.5 pt-4">
+          <div class="like_reaction">
+            @auth
+            @if (Auth::user()->likes()->where('post_id', $post->id)->exists())
+            <div class="flex h-16 w-16 items-center justify-center rounded-xl border"><ion-icon name="heart" class="like-btn cursor-pointer text-pink-500" id="{{$post->id}}_like"></ion-icon></div>
+            @else
+            <div class="flex h-16 w-16 items-center justify-center rounded-xl border"><ion-icon name="heart-outline" class="like-btn cursor-pointer" id="{{$post->id}}_like"></ion-icon></div>
+            @endif
+            <p class="count-num text-center">{{ $post->likes()->sum('count') }}</p>
+            @endauth
+            <p class="mt-1 text-center text-sm">わかる</p>
+          </div>
+          <div class="support_reaction">
+            @auth
+            @if (Auth::user()->supports()->where('post_id', $post->id)->exists())
+            <div class="flex h-16 w-16 items-center justify-center rounded-xl border"><ion-icon name="flame" class="support-btn cursor-pointer text-pink-500" id="{{$post->id}}_support"></ion-icon></div>
+            @else
+            <div class="flex h-16 w-16 items-center justify-center rounded-xl border"><ion-icon name="flame-outline" class="support-btn cursor-pointer" id="{{$post->id}}_support"></ion-icon></div>
+            @endif
+            <p class="count-num text-center">{{ $post->supports()->sum('count') }}</p>
+            @endauth
+            <p class="mt-1 text-center text-sm">応援する</p>
+          </div>
+        </div>
+      </div>
 
-  <footer>
-    <a href="/posts">戻る</a>
-  </footer>
+      <div class="flex-1/2 p-4 pl-8">
+        <h2 class="text-xl font-medium">Comment</h2>
+        <form action="/posts/reply" method="POST">
+          @csrf
+          <div class="comment">
+            <input type="text" name="post_reply[comment]" placeholder="コメント入力" value="{{ old('post_reply.comment') }}">
+            <input type="hidden" name="post_reply[post_id]" value="{{ $post->id }}">
+            <p class="comment_error" style="color:red">{{ $errors->first('post_reply.comment') }}</p>
+          </div>
+          <input class="cursor-pointer rounded-lg bg-black p-1 text-white" type="submit" value="Comment">
+        </form>
 
-  <!-- ioniconインストール -->
-  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <div class="post_reply mt-4 h-48 overflow-y-scroll divide-y divide-gray-300">
+          @foreach ($post->post_replies as $post_reply)
+          <div class="reply__content p-4">
+            <div class="flex gap-2.5 text-xs">
+              <p>profile-pic</p>
+              <p>{{ $post_reply->user->name }}</p>
+              <p>{{ $post_reply->user->country->name }}</p>
+              <p>{{ $post_reply->user->study_abroad_start_date }}</p>
+            </div>
+            <p class="text-sm mt-2">{{ $post_reply->comment }}</p>
+          </div>
+          @endforeach
+        </div>
+      </div>
+    </main>
 
+    <footer>
+      <a href="/posts" class="fixed bottom-16 left-16"><ion-icon name="chevron-back-circle-outline" size="large"></ion-icon></a>
+    </footer>
+
+    <script>
+      function deletepost(id) {
+        'use strict'
+
+        if (confirm('本当に削除しますか？')) {
+          document.getElementById(`form_${id}`).submit();
+        }
+      }
+    </script>
+
+    <!-- ioniconインストール -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  </x-app-layout>
 </body>
 
 </html>
