63625e16a6a5e3f76e21d5b54d3edd02afbdac3f
yurihuku <mochizuki115sannko.gmail.com>
2025-02-27 05:49:30 +0000

    remove links from questions index

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -59,8 +59,6 @@
           </div>
           @endforeach
         </div>
-        <!-- ページ -->
-        <!-- <div class="paginate">{{ $questions->links() }}</div> -->
       </div>
     </main>
     <!-- <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
